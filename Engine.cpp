a2ad431ce953da23bb7e47a5499f4539d54448d9
jdbrice <jdb12@rice.edu>
2017-07-12 15:39:14 -0500

    initial

---
+++ Engine.cpp
@@ -0,0 +1,34 @@
+
+
+// RooBarb
+#include "XmlConfig.h"
+#include "TaskEngine.h"
+using namespace jdb;
+
+// STL
+#include <iostream>
+#include <exception>
+
+
+#include "MVA/MVA.h"
+
+#define LOGURU_IMPLEMENTATION 1
+#include "vendor/loguru.h"
+
+int main( int argc, char* argv[] ) {
+	loguru::add_file("everything.log", loguru::Truncate, loguru::Verbosity_MAX);
+
+
+	cout << "args: ";
+	for ( int i = 0; i < argc; i++ ){
+		cout << "argv[" << i << "] = "<< quote( argv[i] ) << endl;
+	}   
+	Logger::setGlobalLogLevel( "none" );
+
+	TaskFactory::registerTaskRunner<MVA>( "MVA" );
+
+
+	TaskEngine engine( argc, argv );
+	engine.setDefaultTask( "MVA" );
+	return 0;
+}
---
+++ modules/MVA/MVA.h
@@ -0,0 +1,77 @@
+#ifndef MVA_H
+#define MVA_H
+
+
+#include "HistoAnalyzer.h"
+#include "XmlRange.h"
+
+#include "TMVA/Factory.h"
+#include "TMVA/Tools.h"
+
+#include "vendor/loguru.h"
+
+
+
+class MVA : public HistoAnalyzer
+{
+public:
+
+	const int DEBUG = 1;
+	MVA() {}
+	~MVA() {}
+
+	virtual void initialize(){
+		HistoAnalyzer::initialize();
+	}
+protected:
+
+	virtual void make() {
+
+		TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", book->getOutputFile(), "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
+
+		TTree * signal_tree = get<TTree>( "FemtoDst", "signal" );
+		TTree * pion_bg_tree = get<TTree>( "FemtoDst", "pion_bg" );
+
+		LOG_F( INFO, "signal tree = %p", signal_tree );
+		LOG_F( INFO, "pion_bg tree = %p", pion_bg_tree );
+
+
+		// factory->AddVariable( "pT := Tracks.mPt", "p_{T}", "GeV/c", 'F' );
+		// factory->AddVariable( "charge := Tracks.mNHitsFit / abs( Tracks.mNHitsFit )", "q", "", 'I' );
+		factory->AddVariable( "dY := MtdPidTraits.mDeltaY", "MTD DeltaY", "cm", 'F' );
+		factory->AddVariable( "dZ := MtdPidTraits.mDeltaZ", "MTD DeltaZ", "cm", 'F' );
+		// factory->AddVariable( "cell := (MtdPidTraits.mMtdHitChan % 12 )", "MTD cell", "", 'I' );
+
+
+		Double_t signalWeight     = 1.0;
+   		Double_t backgroundWeight = 1.0;
+		
+		factory->AddSignalTree    ( signal_tree,     signalWeight     );
+   		factory->AddBackgroundTree( pion_bg_tree, backgroundWeight );
+
+
+		TCut preselect_signal = "Tracks.mPt > 1.0 && Tracks.mPt < 2.0";
+		TCut preselect_bg = "Tracks.mPt > 1.0 && Tracks.mPt < 2.0";
+		factory->PrepareTrainingAndTestTree( preselect_signal, preselect_bg, "nTrain_Signal=500:nTrain_Background=500:nTest_Signal=500:nTest_Background=500:SplitMode=Random:NormMode=NumEvents:!V" );
+
+		// factory->BookMethod( TMVA::Types::kLikelihood, "Likelihood",
+                        //    "H:!V:TransformOutput=False:PDFInterpol=Spline2:NAvEvtPerBin=100" );
+		// factory->BookMethod( TMVA::Types::kBDT, "BDT",
+                        //    "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" );
+
+		// Train MVAs using the set of training events
+		factory->TrainAllMethods();
+
+		// ---- Evaluate all MVAs using the set of test events
+		factory->TestAllMethods();
+
+		// ----- Evaluate and compare performance of all configured MVAs
+		factory->EvaluateAllMethods();
+
+
+	}
+
+};
+
+
+#endif
